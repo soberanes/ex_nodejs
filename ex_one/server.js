3f648e429a9bdacdcaa68e970e21aad3c5fe8026
Paul Soberanes <paulsoberanes@gmail.com>
2012-10-14 21:56:12 -0500

    example 1

---
+++ ex_one/server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+function ejecutar(){
+	function onRequest(req, res){
+		res.writeHead(200, {'Content-Type':'text/html'});
+		var arr = [
+			'<html>',
+			'<head>',
+			'<title>Hello Universe</title>',
+			'<style type="text/css">h1{background:black;padding:10px;color:white;font-family:"Helvetica";}</style>',
+			'</head>',
+			'<body>',
+			'<h1>HELLO UNIVERSE</h1>',
+			'</body>',
+			'</html>'
+		];
+		res.write(arr.join(''));
+		res.end();
+	}
+
+	var server = http.createServer(onRequest).listen(6969);
+	console.log('Servidor corriendo en puerto 6969');
+}
+
+exports.ejecutar = ejecutar;
